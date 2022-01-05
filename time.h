d9965a21978f61bee97479db0ebc3a8de8cfb13b
omarmahmoud123 <97135279+omarmahmoud123@users.noreply.github.com>
2022-01-05 02:10:04 +0200

    edit

---
+++ time.h
@@ -0,0 +1,6 @@
+#ifndef TIME_H_INCLUDED
+#define TIME_H_INCLUDED
+
+void time_dif();
+
+#endif // TIME_H_INCLUDED
---
+++ undo_redo.h
@@ -0,0 +1,7 @@
+#ifndef UNDO_REDO_H_INCLUDED
+#define UNDO_REDO_H_INCLUDED
+void undo(int n, char a[n][n]);
+void redo(int n, char a[n][n]);
+
+
+#endif // UNDO_REDO_H_INCLUDED
